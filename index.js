c8def92df608467a6705e201df05bf00de992d84
Abigale Del Rosario <delrosarioabigale@gmail.com>
2018-04-01 23:50:23 +0800

    dhsuhd

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 var express = require('express');
 var winston = require('winston');
 var app = express();
-require('./app')(app);
+var func = require('./app');
+func(app);
 
 app.listen(app.get('port'), () => {
     winston.info(`Irregular Please is on PORT:${app.get('port')}`);
